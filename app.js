08b4a307f61b0142b24915423ee76c921210131d
ellisken <ellisken@oregonstate.edu>
2018-08-03 14:23:52 -0700

    finished problem 6 and fixed previous 3 problems to omit use of * and + operators

--- app.js
+++ app.js
@@ -46,15 +46,20 @@ separate arguments and returns an array where the first element is the
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 
-IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. 
+To do addition, use your sum() function, and to do multiplication, 
+use your multiply() function that you've already created. 
+You're going to have to be resourceful to figure out how to do this.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  var sum = a + b + c;
-  var product = a * b * c;
-  return [sum, product, `${a} and ${b} and ${c} sum to ${sum}.`, `The product of ${a} and ${b} and ${c} is ${product}.`]; 
+  var firstSum = sum(a, b);
+  var total = sum(firstSum[0], c)[0];
+  var firstProduct = multiply(a, b);
+  var product = multiply(firstProduct[0], c)[0];
+  return [total, product, `${a} and ${b} and ${c} sum to ${total}.`, `The product of ${a} and ${b} and ${c} is ${product}.`]; 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
@@ -71,22 +76,28 @@ EXACTLY follows this example and uses the values that were input into the functi
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. 
+To do addition, use your sum() function that you've already created. 
+You're going to have to be resourceful to figure out how to do this.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, 
+and when you think it is finished, uncomment the call for the testSumArray() 
+function and see if the test passes.*/
 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-  var summedArrayTuple;
-  var sum = 0;
+  //Set total equal to first number in the array
+  var total = sumArr[0];
 
-  for(var i = 0; i < sumArr.length; i++){
-    sum += sumArr[i];
+  //Else, add the next index for all remaining numbers in the array
+
+  for(var i = 1; i < sumArr.length; i++){
+    total = sum(total, sumArr[i])[0];
   }
 
-  return [sum, `${sumArr.join()} was passed in as an array of numbers, and ${sum} is their sum.`];
+  return [total, `${sumArr.join()} was passed in as an array of numbers, and ${total} is their sum.`];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
@@ -104,16 +115,20 @@ uses the values that were input into the function:
 
 "The numbers 2,3,4 have a product of 24."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. This function should handle an array containing three elements.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. 
+To do multiplication, use your multiply() function that you've already created. 
+You're going to have to be resourceful to figure out how to do this. 
+This function should handle an array containing three elements.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-  var product = 1;
+  var product = multArr[0]; //Set product to the first number in the array
 
-  for(var i = 0; i < multArr.length; i++){
-    product *= multArr[i];
+  //Continue to multiply the product by each remaining number in the array
+  for(var i = 1; i < multArr.length; i++){
+    product = multiply(product, multArr[i])[0];
   }
 
   return [product, `The numbers ${multArr.join()} have a product of ${product}.`]
@@ -130,24 +145,39 @@ testMultiplyArray(testArray);
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers 
+of any length as its argument and returns an array whose first element 
+is the product of those numbers, and the second element is a string that 
+EXACTLY follows this example and concatenates a message using the 
+arguments that were passed into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. 
+To do multiplication, use your multiply() function that you've already created. 
+You're going to have to be resourceful to figure out how to do this.
 
 This function should be dynamic, accepting an array of any length.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, 
+and when you think it is finished, uncomment the call for the 
+testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+  var product = dynamicArray[0]; //Set product to the first number in the array
 
+  //Continue to multiply the product by each remaining number in the array
+  for(var i = 1; i < dynamicArray.length; i++){
+    product = multiply(product, dynamicArray[i])[0];
+  }
+
+  return [product, `The numbers ${dynamicArray.join()} have a product of ${product}.`]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
